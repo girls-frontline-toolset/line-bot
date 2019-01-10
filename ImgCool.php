754bcfef347995260e9f48faea485e6dc93c9bb3
andy19974a22 <andy19974a22@gmail.com>
2019-01-10 15:57:55 +0800

    Add - LICENSE

--- ImgCool.php
+++ ImgCool.php
@@ -3,34 +3,52 @@
 $str = $_GET['str'];
 $zoom = isset($_GET['t'])? 0.25 : 1 ;
 
-if (strLen($str) < 3 ){
-    exit();
-}
-
-//$str .=  "\n。";
-
-
 $imgPath = "img/955602.png";
 
 $bigImg = imagecreatetruecolor(960 * $zoom, 655 * $zoom);
 $typeImg = imagecreatefromstring(file_get_contents($imgPath));
 imagecopyresized ($bigImg, $typeImg , 0, 0, 0, 0, 960 * $zoom, 655 * $zoom,960 , 655 );
 
-//imagefilledrectangle($bigImg, 0, 0, 245, 257, $text_color);
-
-//$text_box = imagettfbbox(40,0,'AdobeFanHeitiStd-Bold.otf',$str);
-
-//$text_width = $text_box[2]-$text_box[0];
-//$x = (242/2) - ($text_width/2);
-
 $color = imagecolorallocate($bigImg, 124, 128, 111);
+$string = ["真","香","。"];
+switch (mb_strlen($str)){
+    case 3:
+        $string = [
+        mb_substr($str, 0,1,"UTF-8"),
+        mb_substr($str, 1,1,"UTF-8"),
+        mb_substr($str, 2,1,"UTF-8")];
+        break;
+    case 2:
+        $string = [
+            mb_substr($str, 0,1,"UTF-8"),
+            mb_substr($str, 1,1,"UTF-8"),
+            "。"];
+        break;
+    case 1:
+        $string = [
+            mb_substr($str, 0,1,"UTF-8"),
+            "。",
+            ""];
+        break;
+    case 0:
 
-imagettftext($bigImg, 60 * $zoom, 0,  775 * $zoom, 420* $zoom, $color, 'AdobeFanHeitiStd-Bold.otf', mb_substr($str, 0,1,"UTF-8"));
-imagettftext($bigImg, 60 * $zoom, 0,  775 * $zoom, 510* $zoom, $color, 'AdobeFanHeitiStd-Bold.otf', mb_substr($str, 1,1,"UTF-8"));
-imagettftext($bigImg, 60 * $zoom, 0,  775 * $zoom , 590* $zoom, $color, 'AdobeFanHeitiStd-Bold.otf', "。");
+
+        break;
+    default:
+        $string = [
+            mb_substr($str, 0,1,"UTF-8"),
+            mb_substr($str, 1,1,"UTF-8"),
+            "。"];
+        break;
+}
 
 
+imagettftext($bigImg, 60 * $zoom, 0,  775 * $zoom, 420* $zoom, $color, 'AdobeFanHeitiStd-Bold.otf', $string[0]);
+imagettftext($bigImg, 60 * $zoom, 0,  775 * $zoom, 510* $zoom, $color, 'AdobeFanHeitiStd-Bold.otf', $string[1]);
+imagettftext($bigImg, 60 * $zoom, 0,  775 * $zoom , 590* $zoom, $color, 'AdobeFanHeitiStd-Bold.otf',$string[2] );
 
+
+header('Access-Control-Allow-Origin: *');
 header('Content-Type: image/jpeg');
 imagejpeg($bigImg, NULL, 100);
 
--- README.md
+++ README.md
@@ -1,74 +1,21 @@
-# girls frontline line bot
+# girls-frontline-toolset / line bot
 
-1. [關於](#about)
-2. [教學](#help)
+## 1. 關於
+一個Line平台回應機械人。支援查詢人型，裝備及妖精製造時間、圖片找尋、真香圖片生成及關鍵字回應。
 
-<a name="about"></a>
-## 關於
 
-版本:V1.3
+## 2. 教學
+[指令表](https://www.ntw-20.com/bot/line)
+[梗圖表](https://www.ntw-20.com/bot/line/img)
 
-問題回報及建議:
+
+## 3.問題回報及建議
 
 https://www.ntw-20.com/more/feedback
 
-網頁版: https://www.ntw-20.com/
+## 4. 版本
+V1.3
 
-作者:從車站用血換了個蘿莉
-
-禁止用作商業用途
-
-
-<a name="help"></a>
-## 教學
-----教學----
-
-查詢方法
-
-### 人型製造時間
-
-#gl g 1 10
-
-#少前 g 1 10
-
-### 妖精製造時間
-
-#gl f 1 10
-
-#少前 f 5 30
-
-### 裝備製造時間
-
-#gl d 52
-
-#少前 d 52
-
-### 圖片找尋(隨機)
-
-#gl i
-
-#少前 i
-
-### 圖片找尋(角色)
-
-#gl i idw
-
-#少前 i idw
-
-### 名字清單
-
-#gl nl SMG 5
-
-#少前 nl SMG 5
-
-### ----關於---
-
-#gl info
-
-#少前 info
-
-### ----網頁---
-
-https://www.ntw-20.com/bot/line
-
+## 5. License
+[MIT](https://github.com/girls-frontline-toolset/line-bot/blob/master/LICENSE)
 
